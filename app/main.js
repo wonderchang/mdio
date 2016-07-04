41b5d705b1cd67bb7fcd3dc7ae69615da9c68a13
Wonder Chang <iwonder.tw@gmail.com>
2016-07-05 00:57:00 +0800

    Remove the console statement

--- app/main.js
+++ app/main.js
@@ -3,6 +3,5 @@ App = require('./app.ls');
 
 subject = decodeURIComponent(location.search).split('?')[1]
 baseUrl = location.origin+location.pathname+'?'+subject
-console.log(location)
 doc = require('./story/'+subject+'/book.md');
 app = new App(doc, baseUrl);
