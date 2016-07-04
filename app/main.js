e7a77cf3ef1384e66280d57f91c82daaf157d2df
Wonder Chang <iwonder.tw@gmail.com>
2016-07-05 00:54:21 +0800

    Enable to get chinese url as parameter

--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
 require('./app.styl');
 App = require('./app.ls');
 
-subject = location.search.split('?')[1]
-doc = require('./script/'+subject+'/main.md');
+subject = decodeURIComponent(location.search).split('?')[1]
 baseUrl = location.origin+location.pathname+'?'+subject
+console.log(location)
+doc = require('./story/'+subject+'/book.md');
 app = new App(doc, baseUrl);
